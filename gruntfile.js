0dfb39a80fbcca9ed9c3695d109da3ef3867476b
Maciej Duda <maciej.duda1@gazeta.pl>
2017-11-29 16:50:02 +0100

    export z kodilli

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+﻿module.exports = function(grunt) {
+grunt.initConfig({
+	jshint: {
+		all: ['js/*.js']
+    },
+
+    browserSync: {
+    	dev: {
+     		bsFiles: {
+        		src : [
+            		'css/*.css',
+            		'./*.html',
+            		'js/*.js',
+            	]
+     		},
+			options: {
+        		watchTask: true,
+        		server: './'
+     		}
+    	}
+	},
+   
+	sass: {
+    	options: {
+    		sourceMap: true
+    	},
+    	dist: {
+    		files: {
+       			'css/style.css': 'sass/style.scss'
+    		}
+    	}
+  	},
+
+	watch: {
+		scripts: {
+        	files: ['sass/*.scss'],
+  			    tasks: ['sass'],
+        		options: {
+       			    spawn: false,
+        		},
+    	}
+	}
+});
+// Load the plugins tasks
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-browser-sync');
+grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+// Default task(s).
+grunt.registerTask('default', [ 'jshint', 'browserSync','sass', 'watch']);
+
+};
